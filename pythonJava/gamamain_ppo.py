c3c32ce9695e9e62568941ce8f75655eaea3178a
meritxell.vinyals <meritxell.vinyals@inrae.fr>
2022-06-19 02:14:26 +0200

    Fix printing headers to delete ceco

--- pythonJava/gamamain_ppo.py
+++ pythonJava/gamamain_ppo.py
@@ -257,7 +257,7 @@ if __name__ == "__main__":
           pass
     #First line contains the title
     with open(results4_filepath, 'a') as f:
-          f.write('iteration,decision_step,budget_obs,fadopters_obs,ceco,cman,cenv,cleft,mean_thetaeco,mean_thetaman,mean_thetaenv,std_thetaeco,std_thetaman,std_thetaenv\n')
+          f.write('iteration,decision_step,budget_obs,fadopters_obs,cman,cenv,cleft,mean_thetaeco,mean_thetaman,mean_thetaenv,std_thetaeco,std_thetaman,std_thetaenv\n')
     
     actor_model = utils.mlp(n_observations, layers_sizes, activation = activation_function)
     print('actor_model.summary()')
