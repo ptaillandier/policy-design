91808a71044db46b31199fcf082bf7d61a7aad07
meritxell.vinyals <meritxell.vinyals@inrae.fr>
2022-09-06 11:21:25 +0200

    Add the simple tests on ppo and vanilla in a script.

---
+++ pythonJava/test_ppo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python gamamain_ppo.py --num-iters 1 --num-batch-episodes 2 --num-mini-batches 2
---
+++ pythonJava/test_vanilla.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python gamamain_vanilla.py --num-iters 1 --num-batch-episodes 2
